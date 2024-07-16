a258dcf9ea57e2707d598b1444260ec6c98674e4
sophie-rud <sophie.rudio@lapiscine.pro>
2024-07-16 16:30:10 +0200

    Refactorisation IndexController

--- controller/indexController.php
+++ controller/indexController.php
@@ -1,16 +1,25 @@
 <?php
+class IndexController {
 
-require_once('../config/config.php');
+    public function index() {
+        require_once('../config/config.php');
 
-$dbConnection = new DbConnection();
-$pdo = $dbConnection -> connect();
+        // on établit la connexion avec la bdd
+        $dbConnection = new DbConnection();
+        $pdo = $dbConnection -> connect();
+        
+        // query() récupère des données de la bdd | ici on sélectionne toutes les colonnes de la table article de la bdd
+        $stmt = $pdo->query("SELECT * FROM article");
+        // nous retourne un tableau avec toutes les données précédemment sélectionnées
+        $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        require_once('../template/page/indexView.php');
+    }
 
-// * sélectionne toutes les colonnes
-$stmt = $pdo->query("SELECT * FROM article");
+}
 
-// nous retourne un tableau avec toutes les données articles
-$articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// création d'une nouvelle instance de la classe IndexController
+$indexController = new IndexController;
+$indexController->index();
 
-
-require_once('../template/page/indexView.php');
 
